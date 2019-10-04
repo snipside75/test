a524212e78e9eed670d6af0d83af1317463b7fcd
Amir Nazarbeigi <amir.nazarbeigi@heyday.fi>
2019-10-04 13:43:01 +0300

    second commit

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import unittest
+import app
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!')
+
+
+if __name__ == '__main__':
+    unittest.main()
